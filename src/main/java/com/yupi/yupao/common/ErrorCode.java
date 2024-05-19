a283f25c2246d2c7010d66680769c36f8f68f878
quanhui <757521740@qq.com>
2024-05-19 10:29:35 +0800

    MyTeam-删除注解

--- src/main/java/com/yupi/yupao/common/ErrorCode.java
+++ src/main/java/com/yupi/yupao/common/ErrorCode.java
@@ -3,12 +3,9 @@ package com.yupi.yupao.common;
 /**
  * 错误码
  *
- * 
- * 
  */
 public enum ErrorCode {
 
-    // https://yupi.icu/
 
     SUCCESS(0, "ok", ""),
     PARAMS_ERROR(40000, "请求参数错误", ""),
